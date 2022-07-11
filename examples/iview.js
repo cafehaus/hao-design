76b5643c8bcacce17f0a2164a56767e5d90df617
ZHOU <510878689@qq.com>
2022-07-11 17:42:59 +0800

    perf：优化配置

--- examples/iview.js
+++ examples/iview.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
-import {
-  Modal,
-  Page,
-} from 'view-design'
+import ViewUI from 'view-design'
+Vue.use(ViewUI)
 
-Vue.component('Modal', Modal)
-Vue.component('Page', Page)
+// 测试组件，不需要按需引入
+// import {
+//   Modal,
+//   Page,
+// } from 'view-design'
+
+// Vue.component('Modal', Modal)
+// Vue.component('Page', Page)
